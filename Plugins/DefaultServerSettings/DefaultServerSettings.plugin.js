508312c5ed4f33b8f558d146493104f388b020a3
Ekibunnel <dorian_live@live.fr>
2021-09-13 01:17:02 +0200

    Add files via upload

---
+++ Plugins/DefaultServerSettings/DefaultServerSettings.plugin.js
@@ -0,0 +1,10 @@
+/**
+ * @name DefaultServerSettings
+ * @author Ekibunnel
+ * @version 0.0.1
+ * @description Set custom settings when joigning a server for the first time.
+ * @invite PEsMUjatGu
+ * @website https://github.com/Ekibunnel/BetterDiscordAddons/
+ * @source https://github.com/Ekibunnel/BetterDiscordAddons/blob/main/Plugins/DefaultServerSettings
+ * @updateUrl https://raw.githubusercontent.com/Ekibunnel/BetterDiscordAddons/main/Plugins/DefaultServerSettings/DefaultServerSettings.plugin.js
+ */
