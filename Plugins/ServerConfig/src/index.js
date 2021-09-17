435977bc0ead8573d990518badb2b4e246ea68c2
Ekibunnel <dorian_live@live.fr>
2021-09-17 23:26:35 +0200

    Add files via upload

--- Plugins/ServerConfig/src/index.js
+++ Plugins/ServerConfig/src/index.js
@@ -1,33 +1,57 @@
+
 module.exports = (Plugin, Library) => {
 
-    const {Logger, Patcher} = Library;
+    const {PluginUtilities} = Library;
+    let dirtyDispatch = BdApi.findModuleByProps("dirtyDispatch");
 
-    return class ExamplePlugin extends Plugin {
+    function ON_GUILD_CREATED(data){
 
-        onStart() {
-            Logger.log("Started");
-            Patcher.before(Logger, "log", (t, a) => {
-                a[0] = "Patched Message: " + a[0];
-            });
+        let settings = PluginUtilities.loadSettings(config.info.name);
+
+        if(Object.keys(settings.notification).length > 0){
+            BdApi.findModuleByProps("updateGuildNotificationSettings").updateGuildNotificationSettings(data.guild.id, settings.notification);
         }
 
+
+        if(settings.nickname.nick){
+            BdApi.findModuleByProps("changeNickname").changeNickname(data.guild.id, null, "@me",  settings.nickname.nick);
+        }
+    }
+
+    function ON_GUILD_JOIGNED(data){
+        let settings = PluginUtilities.loadSettings(config.info.name);
+
+        if(settings.experimental.terms){
+            BdApi.findModuleByProps("submitVerificationForm").submitVerificationForm(data.guildId, "@me");
+        }
+    }
+
+    return class ServerConfig extends Plugin {
+
+        onStart() {
+            if(this.settings.has_seen_settings !== undefined) {
+                BdApi.showToast(`${config.info.name} plugins is running, you have to change the plugin settings to make it do something`,
+                    {
+                        type:"success",
+                        icon:true,
+                        timeout:13000
+                    }
+                );
+            }
+            dirtyDispatch.subscribe("GUILD_CREATE", ON_GUILD_CREATED);
+            dirtyDispatch.subscribe("GUILD_JOIN_REQUEST_CREATE", ON_GUILD_JOIGNED);
+        }
+        
         onStop() {
-            Logger.log("Stopped");
-            Patcher.unpatchAll();
+            dirtyDispatch.unsubscribe("GUILD_CREATE", ON_GUILD_CREATED);
+            dirtyDispatch.unsubscribe("GUILD_JOIN_REQUEST_CREATE", ON_GUILD_JOIGNED);
         }
 
         getSettingsPanel() {
+            BdApi.setData(config.info.name, 'has_seen_settings', true);
             const panel = this.buildSettingsPanel();
-            panel.append(this.buildSetting({
-                type: "switch",
-                id: "otherOverride",
-                name: "A second override?!",
-                note: "wtf is happening here",
-                value: true,
-                onChange: value => this.settings["otherOverride"] = value
-            }));
             return panel.getElement();
-        }
+		}
     };
 
 };
